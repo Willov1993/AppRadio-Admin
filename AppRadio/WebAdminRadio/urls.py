c723650741ef53d860a9097433712562a77e2a24
jjcrowHawk <jjcrow@espol.edu.ec>
2018-09-24 03:34:34 -0500

    Template de agregar emisora creado (aun falta integracion con google maps)

--- AppRadio/WebAdminRadio/urls.py
+++ AppRadio/WebAdminRadio/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('segmento/agregar', views.agregar_segmento, name="ag_segmento")
+    path('segmento/agregar', views.agregar_segmento, name="ag_segmento"),
+    path('emisora/agregar', views.agregar_emisora, name="agregar_emisora")
 ]
--- AppRadio/WebAdminRadio/views.py
+++ AppRadio/WebAdminRadio/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def login(request):
-    return render(request, 'WebAdminRadio/login.html', {'title': 'Login'})
+    return render(request, 'webAdminRadio/login.html', {'title': 'Login'})
+
+def agregar_emisora(request):
+    return render(request,'webAdminRadio/agregar_emisora.html',{'title': 'Agregar Emisora'})
 
 def agregar_segmento(request):
-    return render(request, 'WebAdminRadio/agregar_segmento.html', {'title': 'Agregar Segmento'})
+    return render(request, 'webAdminRadio/agregar_segmento.html', {'title': 'Agregar Segmento'})
